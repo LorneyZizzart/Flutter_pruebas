684fb69644ca9bbf4eca5e1b938e3534926c7e3d
jhonnySAN <yoniguty2016@gmail.com>
2020-03-18 19:29:13 -0400

    peliculas update

--- peliculas/lib/src/widgets/movie_horizontal.dart
+++ peliculas/lib/src/widgets/movie_horizontal.dart
@@ -22,22 +22,27 @@ class MovieHorizontal extends StatelessWidget {
   List<Widget> _tarjetas(BuildContext context) {
     return peliculas.map((pelicula){
       return Container(
-        margin: EdgeInsets.only(right: 15.0), //Separacion a la derecha de 15px
+        margin: EdgeInsets.only(right: 15.0),
+         //Separacion a la derecha de 15px
         child: Column(
           children: <Widget>[
+            
             ClipRRect(
               borderRadius: BorderRadius.circular(20.0),
               child: FadeInImage(
                 image: NetworkImage(pelicula.getPosterImg()),
                 placeholder: AssetImage('assets/img/no-image.jpg'),
                 fit: BoxFit.cover,
-                height: 160.0,
+                height: 140.0,
               ),
             ),
+            SizedBox(height: 5.0,),
             Text(
               pelicula.title,
               overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.caption)
+              style: Theme.of(context).textTheme.caption
+              
+            )
           ],
         ),
       );
